967d8216da8df4e87a6446729f507e0f1d4c9a1e
Billy Davis <billy.davis@praeses.com>
2016-08-04 23:18:51 -0500

    Added test data.

---
+++ src/Database/scripts/TestData/0001-Test-Employees.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO [dbo].[Employee]
+           ([Id],[UserName],[FirstName],[LastName],[EmailAddress],[Type])
+VALUES
+           (NewId(),'pgibbins','Peter','Gibbons','pgibbins@initech.com','ClearMeasure.Bootcamp.Core.Model.Employee'),
+		   (NewId(),'mbolton','Michael','Bolton','mbolton@initech.com','ClearMeasure.Bootcamp.Core.Model.Employee'),
+		   (NewId(),'samir','Samir','Nagheenanajar','snagheenanajar@initech.com','ClearMeasure.Bootcamp.Core.Model.Employee'),
+		   (NewId(),'blumberg','Bill','Lumbergh','blumbergh@initech.com','ClearMeasure.Bootcamp.Core.Model.Employee'),
+		   (NewId(),'bslydell','Bob','Slydell','bslydell@initech.com','ClearMeasure.Bootcamp.Core.Model.Employee'),
+		   (NewId(),'bporter','Bob','Porter','bporter@initech.com','ClearMeasure.Bootcamp.Core.Model.Employee');
+		   
